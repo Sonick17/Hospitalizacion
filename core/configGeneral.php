36906d2d80d5bf71ab1f6ca4604ff66dcc303dde
Leks <asalazar3991@gmail.com>
2020-07-28 21:53:47 -0500

    Hospitalizacion_v6

--- core/configGeneral.php
+++ core/configGeneral.php
@@ -1,4 +1,5 @@
 <?php
-	const SERVERURL="http://localhost:8080/Hospitalizacion/";
+	//const SERVERURL="http://localhost:8080/Hospitalizacion/";
+	const SERVERURL="http://vhospitalizacion/";
 	const COMPANY="SISTEMA DE CAMAS CLINICA";
 	date_default_timezone_set ("America/Lima"); 
